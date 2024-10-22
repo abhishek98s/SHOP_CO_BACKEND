c08eeca2959e8bfb8affeaacdf122518394894d1
abhishek98s <abhishekshakya98.com@gmail.com>
2024-10-22 20:18:29 +0545

    upadet: swagger url

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,92 +1,1 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import validator from 'validator';
-
-import { uploadImage } from '../entities/image/image.controller';
-import { authExceptionMessages } from './constant/authExceptionMessages';
-import { addUser, getUserByEmail } from '../entities/user/user.service';
-import { StatusCodes } from 'http-status-codes';
-
-export const loginHandler = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      throw new Error(authExceptionMessages.EMAIL_PASS_REQUIRED);
-    }
-
-    const user = await getUserByEmail(email);
-
-    const { username, id, phone, email: dbEmail } = user;
-
-    const passordMatched: boolean = await bcrypt.compare(
-      password,
-      user.password,
-    );
-
-    if (!passordMatched) {
-      throw new Error(authExceptionMessages.INVALID_ID_CREDENTIALS);
-    }
-
-    const token = jwt.sign(
-      { id, username, phone, email: dbEmail },
-      process.env.JWT_TOKEN as string,
-    );
-
-    res.status(StatusCodes.OK).json({ data: token });
-  } catch (error) {
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ msg: (error as Error).message });
-  }
-};
-
-export const registerHandler = async (req: Request, res: Response) => {
-  try {
-    const { username, email, password, role, phone } = req.body;
-
-    if (!username || !email || !password || !phone) {
-      throw new Error(authExceptionMessages.USER_CREDENTIALS);
-    }
-
-    if (!validator.isEmail(email)) {
-      throw new Error(authExceptionMessages.INVALID_EMAIL);
-    }
-
-    if (
-      !validator.isStrongPassword(password, {
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-      })
-    ) {
-      throw new Error(authExceptionMessages.PASS_VALIDATION);
-    }
-
-    let imageUrl;
-
-    if (req.file) {
-      const imagePath = req.file!.path;
-      imageUrl = await uploadImage(imagePath);
-    }
-
-    const savedUser = await addUser({
-      username,
-      email,
-      password,
-      phone,
-      role: role ? role : 'user',
-      image_url: imageUrl ? imageUrl : '',
-      created_by: username,
-      updated_by: username,
-    });
-
-    res.json({ success: true, data: savedUser });
-  } catch (error: unknown) {
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ msg: (error as Error).message });
-  }
-};
+import { Request, Response } from 'express';import bcrypt from 'bcrypt';import jwt from 'jsonwebtoken';import validator from 'validator';import { uploadImage } from '../entities/image/image.controller';import { authExceptionMessages } from './constant/authExceptionMessages';import { addUser, getUserByEmail } from '../entities/user/user.service';import { StatusCodes } from 'http-status-codes';export const loginHandler = async (req: Request, res: Response) => {  try {    const { email, password } = req.body;    if (!email || !password) {      throw new Error(authExceptionMessages.EMAIL_PASS_REQUIRED);    }    const user = await getUserByEmail(email);    const { username, id, phone, email: dbEmail } = user;    const passordMatched: boolean = await bcrypt.compare(      password,      user.password,    );    if (!passordMatched) {      throw new Error(authExceptionMessages.INVALID_ID_CREDENTIALS);    }    const token = jwt.sign(      { id, username, phone, email: dbEmail },      process.env.JWT_TOKEN as string,    );    res.status(StatusCodes.OK).json({ success: true, data: token });  } catch (error) {    res      .status(StatusCodes.INTERNAL_SERVER_ERROR)      .json({ msg: (error as Error).message });  }};export const registerHandler = async (req: Request, res: Response) => {  try {    const { username, email, password, role, phone } = req.body;    if (!username || !email || !password || !phone) {      throw new Error(authExceptionMessages.USER_CREDENTIALS);    }    if (!validator.isEmail(email)) {      throw new Error(authExceptionMessages.INVALID_EMAIL);    }    if (      !validator.isStrongPassword(password, {        minLength: 8,        minLowercase: 1,        minUppercase: 1,        minNumbers: 1,      })    ) {      throw new Error(authExceptionMessages.PASS_VALIDATION);    }    let imageUrl;    if (req.file) {      const imagePath = req.file!.path;      imageUrl = await uploadImage(imagePath);    }    await addUser({      username,      email,      password,      phone,      role: role ? role : 'user',      image_url: imageUrl ? imageUrl : '',      created_by: username,      updated_by: username,    });    res.json({ success: true, message: 'Registered Successfully' });  } catch (error: unknown) {    res      .status(StatusCodes.INTERNAL_SERVER_ERROR)      .json({ msg: (error as Error).message });  }};
--- src/entities/user/user.model.ts
+++ src/entities/user/user.model.ts
@@ -1,13 +1,1 @@
-export interface UserModel {
-  id?: string;
-  username: string;
-  email: string;
-  password: string;
-  phone: string;
-  image_url: string;
-  role: 'seller' | 'user' | 'admin';
-  updated_by?: string;
-  created_by?: string;
-  updated_at?: string;
-  created_at?: string;
-}
+export interface UserModel {  id?: string;  username: string;  email: string;  password: string;  phone: string;  image_url: string;  role: 'seller' | 'user' | 'admin';  updated_by?: string;  created_by?: string;  updated_at?: string;  created_at?: string;}
