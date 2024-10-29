f21e0b66e01b488e5d09801b44e210aecdca44c5
abhishek98s <abhishekshakya98.com@gmail.com>
2024-10-29 14:13:47 +0545

    add: migation files

--- src/entities/product/product.model.ts
+++ src/entities/product/product.model.ts
@@ -2,15 +2,21 @@ export interface IProduct {
     id?: number,
     name: string,
     description: string,
-    rating: string | 0,
+    rating: number | 0,
     price: number,
     discount?: number,
     discounted_price?: number,
-    category: number,
-    style: number,
-    type: number,
-    created_by?: number,
-    updated_by?: number,
+    image_url: string,
+    stock_quantity: number | 0,
+    tags?: string[],
+
+    size_id: number;
+    category_id: number,
+    style_id: number,
+    type_id: number,
+
+    created_by?: string,
+    updated_by?: string,
     created_at?: string,
     updated_at?: string,
 }
---
+++ src/migrations/20241029042438_products.ts
@@ -0,0 +1,39 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('products', (table) => {
+        table.increments('id').primary();
+        table.string('name', 255).notNullable();
+        table.text('description').notNullable();
+        table.float('rating').defaultTo(0);
+        table.decimal('price', 10, 2).notNullable();
+        table.decimal('discount', 10, 2).defaultTo(0);
+        table.decimal('discounted_price', 10, 2);
+        table.string('image_url').notNullable();
+        table.integer('stock_qualtity').notNullable();
+
+        table.integer('size_id').notNullable();
+        table.foreign('size_id').references('id').inTable('sizes').onDelete('CASCADE');
+
+        table.integer('category_id').notNullable();
+        table.foreign('category_id').references('id').inTable('categories').onDelete('CASCADE');
+
+        table.integer('style_id').notNullable();
+        table.foreign('style_id').references('id').inTable('product_styles').onDelete('CASCADE');
+        
+        table.integer('type_id').notNullable();
+        table.foreign('type_id').references('id').inTable('product_types').onDelete('CASCADE');
+        
+        // table.integer('category', ['new_arrival', 'top_selling']);
+        // table.integer('style', ['casual', 'formal', 'party', 'gym']);
+        // table.integer('type', ['t-shirts', 'shorts', 'shirts']);
+
+        table.string('created_by').notNullable();
+        table.string('updated_by').notNullable();
+        table.timestamps(true, true);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists('products');
+}
